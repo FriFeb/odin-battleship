da0c73caca32ac2228c5c20f25d404f9910f8900
Fedir Denysenko <51440204+FriFeb@users.noreply.github.com>
2024-08-06 14:55:33 +0300

    style: improve gameboards and buttons appearance

--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,10 @@
 export const GAMEBOARD_SIZE = 10;
 export const SHIP_DIRECTIONS = ['horizontal', 'vertical'];
-export const GAMEBOAED_SHIP_TYPES = [
-  { length: 4, count: 1 },
-  { length: 3, count: 2 },
-  { length: 2, count: 3 },
-  { length: 1, count: 4 },
-];
+// export const GAMEBOAED_SHIP_TYPES = [
+//   { length: 4, count: 1 },
+//   { length: 3, count: 2 },
+//   { length: 2, count: 3 },
+//   { length: 1, count: 4 },
+// ];
 
-// export const GAMEBOAED_SHIP_TYPES = [{ length: 1, count: 1 }];
+export const GAMEBOAED_SHIP_TYPES = [{ length: 1, count: 1 }];
